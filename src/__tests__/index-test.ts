88959347589c59f2e23bdf2f86dd87a04e952fa6
Vadim Taits <taizvadim@gmail.com>
2024-03-28 12:10:42 +0300

    Eslint -> biome

--- src/__tests__/index-test.ts
+++ src/__tests__/index-test.ts
@@ -1,48 +1,43 @@
-import { jest } from '@jest/globals';
+import { jest } from "@jest/globals";
 
-jest.unstable_mockModule('react', () => ({
-  useRef: jest.fn(),
+jest.unstable_mockModule("react", () => ({
+	useRef: jest.fn(),
 }));
 
-const {
-  useRef,
-} = await import('react');
+const { useRef } = await import("react");
 
-const {
-  useLazyRef,
-  EMPTY_VALUE,
-} = await import('../index');
+const { useLazyRef, EMPTY_VALUE } = await import("../index");
 
 const mockedUseRef = jest.mocked(useRef);
 
-test('should return filled value and not call init', () => {
-  const init = jest.fn();
+test("should return filled value and not call init", () => {
+	const init = jest.fn();
 
-  const refResult = {
-    current: Symbol('result'),
-  };
+	const refResult = {
+		current: Symbol("result"),
+	};
 
-  mockedUseRef.mockReturnValue(refResult);
+	mockedUseRef.mockReturnValue(refResult);
 
-  const result = useLazyRef(init);
+	const result = useLazyRef(init);
 
-  expect(init).toHaveBeenCalledTimes(0);
-  expect(result).toBe(refResult);
+	expect(init).toHaveBeenCalledTimes(0);
+	expect(result).toBe(refResult);
 });
 
-test('should return result of init', () => {
-  const resultValue = Symbol('result');
+test("should return result of init", () => {
+	const resultValue = Symbol("result");
 
-  const init = jest.fn().mockReturnValue(resultValue);
+	const init = jest.fn().mockReturnValue(resultValue);
 
-  mockedUseRef.mockReturnValue({
-    current: EMPTY_VALUE,
-  });
+	mockedUseRef.mockReturnValue({
+		current: EMPTY_VALUE,
+	});
 
-  const result = useLazyRef(init);
+	const result = useLazyRef(init);
 
-  expect(init).toHaveBeenCalledTimes(1);
-  expect(result).toEqual({
-    current: resultValue,
-  });
+	expect(init).toHaveBeenCalledTimes(1);
+	expect(result).toEqual({
+		current: resultValue,
+	});
 });
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,14 @@
-import {
-  useRef,
-} from 'react';
-import type {
-  MutableRefObject,
-} from 'react';
+import { useRef } from "react";
+import type { MutableRefObject } from "react";
 
-export const EMPTY_VALUE = Symbol('useLazyRef empty value');
+export const EMPTY_VALUE = Symbol("useLazyRef empty value");
 
-export const useLazyRef = <T>(
-  init: () => T,
-): MutableRefObject<T> => {
-  const resultRef = useRef<T | typeof EMPTY_VALUE>(EMPTY_VALUE);
+export const useLazyRef = <T>(init: () => T): MutableRefObject<T> => {
+	const resultRef = useRef<T | typeof EMPTY_VALUE>(EMPTY_VALUE);
 
-  if (resultRef.current === EMPTY_VALUE) {
-    resultRef.current = init();
-  }
+	if (resultRef.current === EMPTY_VALUE) {
+		resultRef.current = init();
+	}
 
-  return resultRef as MutableRefObject<T>;
+	return resultRef as MutableRefObject<T>;
 };
